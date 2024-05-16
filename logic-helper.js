8fc0619a39f6f79adcbdf124e8ee475dc7cf8e82
Neo Fanetti <neo.fanetti@swisscom.com>
2024-05-16 15:26:20 +0200

    feat: start implement cell logic

--- logic-helper.js
+++ logic-helper.js
@@ -1,1 +1,22 @@
+import { CELL_COMBINATIONS } from "./constants.js";
+
 export const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const checkBirthCells = (board, nextBoard) => {
+  for (let row = 0; row < board.length; row++) {
+    for (let col = 0; col < board[0].length; col++) {
+      if (board[row][col] !== 0) continue; // only empty cells get checked
+
+      let neighbourCounter = 0;
+
+      CELL_COMBINATIONS.forEach((combination) => {
+        try {
+          if (board[row + combination[0]][col + combination[1]] == 1)
+            neighbourCounter++;
+        } catch (err) {}
+      });
+
+      if (neighbourCounter === 3) nextBoard[row][col] = 1;
+    }
+  }
+};
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { refresh } from "./draw.js";
+import { checkBirthCells } from "./logic-helper.js";
 
 // canvas and context
 const canvas = document.createElement("canvas");
@@ -23,43 +23,107 @@ const GRID_COLS = Math.ceil(canvasWidth / GRID_CELLSIZE);
 // array representing board
 const board = Array.from(Array(GRID_ROWS), () => new Array(GRID_COLS).fill(0));
 
-let row = 0;
-let col = 0;
+board[0][2] = 1;
+board[1][1] = 1;
+board[2][2] = 1;
+// board[1][3] = 1;
+// board[0][3] = 1;
+// board[0][1] = 1;
+console.table(board);
 
-async function update() {
-  // TODO: just call it every 100th frame or so
+// combinations to loop over for each field (8 positions)
+const combinations = [
+  [0, -1],
+  [0, +1],
+  [-1, 0],
+  [-1, -1],
+  [-1, +1],
+  [+1, 0],
+  [+1, -1],
+  [+1, +1],
+];
 
-  if (row == board.length) {
-    row = 0;
-    col++;
-  }
+// deep copy of board for the next iteration
+let nextBoard = board.map((row) => [...row]);
 
-  if (col == board[0].length) {
-    col = 0;
-    row = 0;
-  }
+checkBirthCells(board, nextBoard);
 
-  board[row][col] = 1;
+// for (let row = 0; row < board.length; row++) {
+//   for (let col = 0; col < board[0].length; col++) {
+//     if (board[row][col] !== 0) continue; // only empty cells get checked
 
-  refresh(
-    canvasWidth,
-    canvasHeight,
-    GRID_ROWS,
-    GRID_COLS,
-    board,
-    GRID_CELLSIZE,
-    GRID_COLOR,
-    BLOCK_COLOR,
-    ctx
-  );
+//     let neighbourCounter = 0;
 
-  board[row][col] = 0;
-  row++;
+//     combinations.forEach((combination) => {
+//       try {
+//         if (board[row + combination[0]][col + combination[1]] == 1)
+//           neighbourCounter++;
+//       } catch (err) {}
+//     });
 
-  await wait(1000);
+//     if (neighbourCounter === 3) nextBoard[row][col] = 1;
+//   }
+// }
 
-  console.log("frame updated");
-  requestAnimationFrame(update);
-}
+console.table(nextBoard);
 
-update();
+// combinations
+// board[row][col - 1];
+// board[row][col + 1];
+
+// board[row - 1][col];
+
+// board[row - 1][col - 1];
+// board[row - 1][col + 1];
+
+// board[row + 1][col];
+
+// board[row + 1][col - 1];
+// board[row + 1][col + 1];
+
+// iterate over every element
+// for (let row = 0; row < board.length; row++) {
+//   for (let col = 0; col < board[0].length; col++) {
+//     if (board[row][col] == 1) console.log("indexes: ", row, col);
+//   }
+// }
+
+// let row = 0;
+// let col = 0;
+
+// async function update() {
+//   if (row == board.length) {
+//     row = 0;
+//     col++;
+//   }
+
+//   if (col == board[0].length) {
+//     col = 0;
+//     row = 0;
+//   }
+
+//   board[row][col] = 1;
+
+//   refresh(
+//     canvasWidth,
+//     canvasHeight,
+//     GRID_ROWS,
+//     GRID_COLS,
+//     board,
+//     GRID_CELLSIZE,
+//     GRID_COLOR,
+//     BLOCK_COLOR,
+//     ctx
+//   );
+
+//   board[row][col] = 0;
+//   row++;
+
+//   // time diff between every iteration
+//   await wait(1000);
+
+//   console.log("frame updated");
+//   requestAnimationFrame(update);
+// }
+
+// update();
